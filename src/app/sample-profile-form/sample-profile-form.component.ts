e62561a3ceb60c4098ddc7a3cccc09993877ea1a
Girish Salaskar <girishsalaskar@users.noreply.github.com>
2021-06-08 17:25:29 +0530

    Revert "Readme updated for notes purpose"
    This reverts commit ebc750ac7c1c52883213c364fdd7bb5f4b3fa087.

--- src/app/sample-profile-form/sample-profile-form.component.ts
+++ src/app/sample-profile-form/sample-profile-form.component.ts
@@ -12,26 +12,23 @@ export class SampleProfileFormComponent implements OnInit {
     first_name: new FormControl('', [Validators.required, Validators.minLength(3)]),
     last_name: new FormControl('', [Validators.required, Validators.minLength(3)]),
     phone_number: new FormControl('', [Validators.pattern('[0-9]{8,}'), Validators.required]),
+    address: new FormControl('', [Validators.required, Validators.minLength(3)]),
     email_address: new FormControl('', [Validators.pattern('[a-zA-Z0-9\.\_\@]{7,}'), Validators.required]),
     education: new FormControl(''),
-    address: new FormGroup({
-      landmark: new FormControl('', [Validators.required, Validators.minLength(3)]),
-      country: new FormControl(''),
-      state: new FormControl('')
-    })
+    country: new FormControl(''),
+    state: new FormControl('')
   });;
   constructor() { }
 
   ngOnInit() {
     //valueChange subscribe
-    this.userProfile.controls["first_name"].valueChanges.subscribe((value) => { this.trackChange(value) });
-    console.log(this.userProfile.controls["address"]);
+    //this.first_name.valueChanges.subscribe((value)=>{this.trackChange(value)});
   }
 
   //Value change subscribe listener
   trackChange(value: any) {
     console.log(value);
-    console.log("New Value : " + this.userProfile.controls["first_name"].value);
+    //console.log("New Value : " + this.first_name.value);
   }
 
   submitProfile() {
