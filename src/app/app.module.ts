2a0cb6ef7cfeaffa8a808118aa59701d0ceb20aa
Girish Salaskar <girishsalaskar@users.noreply.github.com>
2021-06-14 16:29:48 +0530

    Routing rolled back

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ExpenseEntryComponent } from './expense-entry/expense-entry.component';
 import { TestingChildComponent } from './testing-child/testing-child.component';
 import { SampleProfileFormComponent } from './sample-profile-form/sample-profile-form.component';
-
-const routs: Routes = [
-  { path:'home',component:AppComponent},
-  { path:'expense-entry',component:ExpenseEntryComponent},
-  { path:'testing-child',component:TestingChildComponent},
-  { path:'sample-profile',component:SampleProfileFormComponent}
-]
 
 @NgModule({
   declarations: [
@@ -23,7 +15,6 @@ const routs: Routes = [
     SampleProfileFormComponent
   ],
   imports: [
-    RouterModule.forRoot(routs),
     BrowserModule,
     FormsModule,
     ReactiveFormsModule
