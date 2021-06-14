630d7e601ddd62ac7cc166bac9f0d4bef6ae22e8
Girish Salaskar <girishsalaskar@users.noreply.github.com>
2021-06-14 16:28:56 +0530

    Routing added in existing project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ExpenseEntryComponent } from './expense-entry/expense-entry.component';
 import { TestingChildComponent } from './testing-child/testing-child.component';
 import { SampleProfileFormComponent } from './sample-profile-form/sample-profile-form.component';
+
+const routs: Routes = [
+  { path:'home',component:AppComponent},
+  { path:'expense-entry',component:ExpenseEntryComponent},
+  { path:'testing-child',component:TestingChildComponent},
+  { path:'sample-profile',component:SampleProfileFormComponent}
+]
 
 @NgModule({
   declarations: [
@@ -15,6 +23,7 @@ import { SampleProfileFormComponent } from './sample-profile-form/sample-profile
     SampleProfileFormComponent
   ],
   imports: [
+    RouterModule.forRoot(routs),
     BrowserModule,
     FormsModule,
     ReactiveFormsModule
