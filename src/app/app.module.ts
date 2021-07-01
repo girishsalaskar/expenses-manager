80cc478b619892685c78846e42d1b38c43f93eb9
Girish Salaskar <girishsalaskar@users.noreply.github.com>
2021-07-01 07:47:36 +0530

    Routing example added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ExpenseEntryComponent } from './expense-entry/expense-entry.component';
 import { TestingChildComponent } from './testing-child/testing-child.component';
 import { SampleProfileFormComponent } from './sample-profile-form/sample-profile-form.component';
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component';
+
+const routs: Routes = [
+  { path:'one',component:OneComponent},
+  { path:'two',component:TwoComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     ExpenseEntryComponent,
     TestingChildComponent,
-    SampleProfileFormComponent
+    SampleProfileFormComponent,
+    OneComponent,
+    TwoComponent
   ],
   imports: [
+    RouterModule.forRoot(routs),
     BrowserModule,
     FormsModule,
     ReactiveFormsModule
